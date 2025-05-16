888f80a91d51736490c9f26a09597ec0e44ca854
видеокал- <dich140804@gmail.com>
2025-05-16 17:43:13 +0300

    Added config.

---
+++ src/config/config.ts
@@ -0,0 +1,4 @@
+export default () => ({
+  appPort: process.env.APP_PORT,
+  appHost: process.env.APP_HOST
+})
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './Modules/App/app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3000)
+
+  const configService = app.get(ConfigService) 
+  await app.listen(configService.get("appPort") ?? 3000)
 }
 bootstrap()
